38749776caf3b18892d28cacf315996aa51b3204
Ynthm Wang <ynthm.w@gmail.com>
2022-10-06 04:47:20 +0800

    big change

---
+++ activiti-demo/src/test/java/com/ynthm/spring/demo/activiti/ActivitiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ynthm.spring.demo.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
--- spring-boot-template/common-web/src/main/java/com/ynthm/common/web/WebConstant.java
+++
@@ -1,8 +0,0 @@
-package com.ynthm.common.web;
-
-/** @author ethan */
-public class WebConstant {
-  private WebConstant() {}
-
-  public static final String ENV_PROD = "prod";
-}
