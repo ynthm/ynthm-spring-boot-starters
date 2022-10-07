df1cd088c55829c5d1973c17a180de77a95cc04c
Ynthm Wang <ynthm.w@gmail.com>
2022-10-07 19:55:04 +0800

    big change

--- mybatis-demo/build.sh
+++
@@ -1,7 +0,0 @@
-env=prod
-if [ -n "$1" ]; then
-  env=$1
-fi
-
-# -Dmaven.test.skip=true  不执行测试用例，也不编译测试用例类至target/test-classes
-mvn clean package -P$env -DskipTests
--- mybatis-demo/src/main/java/com/ynthm/demo/mybatis/user/common/BaseEnum.java
+++
@@ -1,6 +0,0 @@
-package com.ynthm.demo.mybatis.user.common;
-
-/** @author ethan */
-public interface BaseEnum<E extends BaseEnum<?>> {
-  int getValue();
-}
